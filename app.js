32ac4f715d0732fb43e4366080b6f3becd966018
Randy <83686830+ARevampedLifeCoding@users.noreply.github.com>
2021-08-01 22:04:35 -0700

    switched from metric to imperial

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const api = {
     weather_el.innerText = weather.weather[0].main;
   
     let hilow = document.querySelector('.hi-low');
-    hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
+    hilow.innerText = `${Math.round(weather.main.temp_min)}°F / ${Math.round(weather.main.temp_max)}°F`;
   }
   
   function dateBuilder (d) {
